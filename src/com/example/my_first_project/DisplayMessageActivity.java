f580af1f9429a2c240561fb6c5eadb3343c62316
shimon lavion <shemilavion@gmail.com>
2012-10-31 00:28:23 +0200

    add new activity to send button

---
+++ src/com/example/my_first_project/DisplayMessageActivity.java
@@ -0,0 +1,17 @@
+package com.example.my_first_project;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+
+public class DisplayMessageActivity extends Activity 
+{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+
+}
