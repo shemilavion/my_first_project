1d6f6679a6af9bbf2867b72607ddd85db15dc227
shimon lavion <shemilavion@gmail.com>
2012-10-31 15:50:50 +0200

    hadle the sent message in a new activity

--- src/com/example/my_first_project/DisplayMessageActivity.java
+++ src/com/example/my_first_project/DisplayMessageActivity.java
@@ -1,7 +1,9 @@
 package com.example.my_first_project;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.app.Activity;
+import android.content.Intent;
 
 
 public class DisplayMessageActivity extends Activity 
@@ -12,6 +14,18 @@ public class DisplayMessageActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+        //get the intent that created this activity & grab the incoming message from it
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        
+
+        // Create the text view to show the income message
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
     }
 
 }
